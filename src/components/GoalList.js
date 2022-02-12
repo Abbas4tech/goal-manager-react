e8ce12ea8b06d71348b420cc03602c85faec452a
Abbas4tech <shaikhabbas54@gmail.com>
2022-02-12 11:19:35 +0530

    added conditional template

--- src/components/GoalList.js
+++ src/components/GoalList.js
@@ -6,20 +6,26 @@ const GoalList = ({ getGoalData, deleteHandler }) => {
   return (
     <>
       <ul className="flex flex-col items-center h-20 mt-5 w-96">
-        {getGoalData.map((goal) => (
-          <li
-            key={goal.id}
-            className="flex items-center justify-between w-full p-3 m-1 text-white rounded-md bg-slate-400"
-          >
-            <p className="text-2xl ">{goal.goal}</p>
-            <button
-              className="p-2 rounded-md bg-slate-600 hover:bg-slate-900"
-              onClick={() => deleteHandler(goal.id)}
-            >
-              Delete!
-            </button>
+        {getGoalData.length === 0 ? (
+          <li className="flex items-center justify-center w-full p-3 m-1 text-white rounded-md bg-slate-400">
+            <p className="text-xl ">No Goal Found!</p>
           </li>
-        ))}
+        ) : (
+          getGoalData.map((goal) => (
+            <li
+              key={goal.id}
+              className="flex items-center justify-between w-full p-3 m-1 text-white rounded-md bg-slate-400"
+            >
+              <p className="text-xl ">{goal.goal}</p>
+              <button
+                className="p-2 rounded-md bg-slate-600 hover:bg-slate-900"
+                onClick={() => deleteHandler(goal.id)}
+              >
+                Delete!
+              </button>
+            </li>
+          ))
+        )}
       </ul>
     </>
   );
